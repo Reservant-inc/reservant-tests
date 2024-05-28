42909ca4a7de6b8eda46c649d25a7bf551a1c074
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-05-28 22:52:45 +0200

    Update UserGroups.py

--- Selenium/UserGroups.py
+++ Selenium/UserGroups.py
@@ -7,15 +7,26 @@ home_url = f"http://{ip}{home_path}"
 
 
 def test_user_groups(driver):
-
+    info("Logging in user...")
     login(driver, "JD", "Pa$$w0rd")
+    info("User logged in.")
 
-    info("USER HOME TEST")
+    info("USER HOME TEST:\n")
     driver.get(home_url)
 
     try:
         # Sprawdzenie tytułu
         assert "React App" in driver.title, f"Oczekiwano tytułu zawierającego 'React App', otrzymano: {driver.title}"
+
+        # Szukanie przycisku grup
+        wait_for_element(driver, By.ID, "NavbarRestaurantsSectionButton")
+        result("NavbarRestaurantsSectionButton found.")
+
+        # Przechodzenie do groups
+        click_button(driver, By.ID, "NavbarRestaurantsSectionButton")
+
+        wait_for(delay)
+        # TODO: implement groups testing
 
     except Exception as e:
         print("Could not load home webpage")
@@ -24,5 +35,4 @@ def test_user_groups(driver):
 if __name__ == "__main__":
     driver = webdriver.Edge()
     test_user_groups(driver)
-    time.sleep(delay)
     driver.quit()
