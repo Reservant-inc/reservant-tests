e159ecef901288bf5b6993788635c773d4a92d37
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-10-04 13:33:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from Selenium.UserRegister import test_user_register
 from Selenium.UserLogin import test_user_login
 
 
 def main():
-    driver = webdriver.Edge()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
     test_user_register(driver)
     print()
     test_user_login(driver)
