07048586c5777050bf75de64bb96bec51746a28d
s26496 <s26496@pjwstk.edu.pl>
2024-11-17 21:24:56 +0100

    Remove friend, Remove outgoing request tests
    Tests for Removing a friend and removing and an outgoing friend request. Both ffrom user's Friends profile tab.

--- Selenium/RemoveOutgoingRequest.py
+++ Selenium/RemoveOutgoingRequest.py
@@ -1,5 +1,5 @@
-# Usuwa znajomego. Przed każdym odpaleniem należy wykonać recreate database, żeby upewnić się, że
-# KK jest na liście znajomych JB
+# Usuwa wychodzące zaproszenie do znajomych z zakładki Profil > Znajomi.
+# Przed odpaleniem należy wykonać recreate database.
 
 from utils.utils import *
 from Selenium.UserLogin import test_user_login
@@ -13,8 +13,8 @@ login_url = f"http://{ip}{login_path}"
 home_path = get_variable_value("HOME_PATH")
 home_url = f"http://{ip}{home_path}"
 
-def test_remove_friend(driver):
-    info("TEST REMOVING A FRIEND FROM YOUR FRIENDS LIST VIA USER'S PROFILE TAB")
+def test_remove_outgoing_request(driver):
+    info("REMOVE AN OUTGOING FRIEND REQUEST VIA USER'S PROFILE TAB")
     try:
         driver.get(home_url)
         wait_for_url_to_be(driver, home_url)
@@ -35,27 +35,27 @@ def test_remove_friend(driver):
         wait_for_element(driver, By.XPATH, "//h1[contains(text(), 'Friends') or contains(text(), 'Znajomi')]").click()
         wait_for(delay)
 
-        # Szukam elementu listy który zawiera 'Krzysztof Kowalski'
-        # Jeśli nie ma elementu to kończy tekst (trzeba wykonać rebuild db)
-        li_element = wait_for_element(driver, By.XPATH, "//li[contains(., 'Krzysztof') and contains(., 'Kowalski')]",
-                                      critical=True)
-
-        # Szukam diva z przyciskami
-        div_element = li_element.find_element(By.CSS_SELECTOR, "div.flex.gap-4.mt-2")
-
-        # Szukam pierwszy guzik (Odpowiedzialny za usuwanie znajomego)
-        first_child = div_element.find_element(By.XPATH, "./*")
-        first_child.click()
+        # Przełączam zakładkę na 'Wysłane zaproszenia'
+        sent_requests_tab = driver.find_element(By.XPATH,
+                                                "//button[contains(text(), 'Wysłane zaproszenia') or contains(text(), 'Outgoing requests')]")
+        sent_requests_tab.click()
         wait_for(delay)
 
-        # Klikam 'Tak' w Popupie potwierdzającym usunięcie znajomego
-        click_button(driver, By.XPATH, "(//button[contains(text(), 'Tak') or contains(text(), 'Yes')])")
+        # Szukam elementu listy który zawiera 'Geralt Riv'
+        # Jeśli nie ma elementu to kończy tekst (trzeba wykonać recreate database)
+        li_element = wait_for_element(driver, By.XPATH, "//li[contains(., 'Geralt') and contains(., 'Riv')]",
+                                      critical=True)
+
+        # Szukam guzika do cofnięcia zaproszenia
+        button = li_element.find_element(By.CSS_SELECTOR, "button.mt-2.border-\\[1px\\].text-sm.p-2.w-fit.rounded-lg.bg-grey-0.border-primary.text-primary.transition.hover\\:scale-105.hover\\:bg-primary.hover\\:text-white")
+        button.click()
         wait_for(delay)
 
         return True
+
     except Exception as e:
         result(str(e), False)
-        info("Friend removal test failed")
+        info("Friend request removal test failed")
         info(f"Current URL: {driver.current_url}")
         info(f"Page title: {driver.title}")
         return False  # Test nieudany
@@ -68,5 +68,5 @@ if __name__ == "__main__":
     driver = webdriver.Edge()
     driver.maximize_window()
     test_user_login(driver, False)
-    test_remove_friend(driver)
+    test_remove_outgoing_request(driver)
     driver.quit()
